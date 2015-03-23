376d34ca557bc8723651ccc438f5d4f7336af133
Chris Weber <chris.l.weber@gmail.com>
2015-03-23 15:18:28 -0700

    Added checking

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ DNS = {
 }
 
 
-def abort_if_ip_doesnt_exist(name):
+def abort_if_name_doesnt_exist(name):
     if name not in DNS:
         abort(404, message="name {} doesn't exist".format(name))
 
@@ -21,16 +21,19 @@ parser.add_argument('name', type=str)
 
 
 # ip
-#   show a single ip item and lets you delete them
-class ip(Resource):
+#   show a single item and lets you delete them
+class dns(Resource):
     def get(self, name):
-        abort_if_ip_doesnt_exist(name)
+        abort_if_name_doesnt_exist(name)
         return DNS[name]
 
     def delete(self, name):
-        abort_if_ip_doesnt_exist(name)
+        abort_if_name_doesnt_exist(name)
         del DNS[name]
         return '', 204
+
+    def post(self,name):
+        abort(404, message="Name not found")
 
     def put(self, name):
         args = parser.parse_args()
@@ -39,23 +42,35 @@ class ip(Resource):
         return ip, 201
 
 
-# ipList
-#   shows a list of all DNS, and lets you POST to add new ips
-class ipList(Resource):
+# dnsList
+#   shows a list of all Entries , and lets you POST to add new ips
+class dnsList(Resource):
     def get(self):
         return DNS
 
+    def delete(self,name):
+        abort(404, message="DELETE not Allowed")
+
+    def put(self,name):
+        abort(404, message="PUT not Allowed")
+
+
     def post(self):
         args = parser.parse_args()
+        if (args['name'] is None or args['ip'] is None):
+            abort(404,message="Name and ip required")    
         name = args['name']
+        if name in DNS:
+            abort(404,message="Item already exists name:"+str(name))
+
         DNS[name] = {'ip': args['ip']}
         return DNS[name], 201
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(ipList, '/dns')
-api.add_resource(ip, '/dns/<name>')
+api.add_resource(dnsList, '/dns')
+api.add_resource(dns, '/dns/<name>')
 
 
 if __name__ == '__main__':
