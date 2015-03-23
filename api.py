d616809737051ac2cee16d1d1eae54aaf8bd57b2
Chris Weber <chris.l.weber@gmail.com>
2015-03-23 14:34:11 -0700

    Initial commit

---
+++ api.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask.ext.restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+DNS = {
+    'google.com': {'ip': '173.194.33.179'},
+    'microsoft.com': {'ip': '134.170.188.221'},
+    'urbanairship.com': {'ip': '216.223.21.212'},
+}
+
+
+def abort_if_ip_doesnt_exist(name):
+    if name not in DNS:
+        abort(404, message="name {} doesn't exist".format(name))
+
+parser = reqparse.RequestParser()
+parser.add_argument('ip', type=str)
+parser.add_argument('name', type=str)
+
+
+# ip
+#   show a single ip item and lets you delete them
+class ip(Resource):
+    def get(self, name):
+        abort_if_ip_doesnt_exist(name)
+        return DNS[name]
+
+    def delete(self, name):
+        abort_if_ip_doesnt_exist(name)
+        del DNS[name]
+        return '', 204
+
+    def put(self, name):
+        args = parser.parse_args()
+        ip = {'ip': args['ip']}
+        DNS[name] = ip
+        return ip, 201
+
+
+# ipList
+#   shows a list of all DNS, and lets you POST to add new ips
+class ipList(Resource):
+    def get(self):
+        return DNS
+
+    def post(self):
+        args = parser.parse_args()
+        name = args['name']
+        DNS[name] = {'ip': args['ip']}
+        return DNS[name], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(ipList, '/dns')
+api.add_resource(ip, '/dns/<name>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
